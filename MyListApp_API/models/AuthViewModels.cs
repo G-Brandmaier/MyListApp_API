9821c0c396bccf958d0ce55ca8b6c6656a312928
riaemi <riaem@LAPTOP-AJ2FA1L2>
2023-10-20 15:57:42 +0200

    Add AccountController med Register funktion

---
+++ MyListApp_API/models/AuthViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyListApp_API.Models
+{
+    public class AuthViewModels
+    {
+        public class RegisterViewModel
+        {
+            [Required]
+            [EmailAddress]
+            public string? Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+            public string? Password { get; set; }
+        }
+
+        public class LoginViewModel
+        {
+            [Required]
+            [EmailAddress]
+            public string? Email { get; set; }
+
+            [Required]
+            public string? Password { get; set; }
+        }
+    }
+}
+
---
+++ MyListApp_API/services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using MyListApp_API.Models;
+
+namespace MyListApp_API.Services
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterUserAsync(AuthViewModels.RegisterViewModel model); // Interface method for user registration
+        Task<User> AuthenticateAsync(string email, string password); // Interface method for user authentication
+
+    }
+}
