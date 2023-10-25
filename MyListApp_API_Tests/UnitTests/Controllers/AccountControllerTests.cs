4b076f7327434a3eb5cdb96539d3e342271a32f2
riaemi <riaem@LAPTOP-AJ2FA1L2>
2023-10-25 09:27:32 +0200

    AddTestsKlasser

---
+++ MyListApp_API_Tests/UnitTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyListApp_API_Tests.UnitTests.Controllers
+{
+    internal class AccountControllerTest
+    {
+    }
+}
