be1e6f521320e19f2a1101cf64d19b8a189b0244
Ghazeleh Beheshtian <Ghazeleh Beheshtian@LAPTOP-T0U167JL>
2023-10-30 16:27:39 +0100

    Update min program

--- MyListApp_API/Program.cs
+++ MyListApp_API/Program.cs
@@ -7,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IListService, ListService>();
 builder.Services.AddSingleton<IListRepo, ListRepo>();
 builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddSingleton<UserRepo>(); //register a UserRepo
 builder.Services.AddSingleton<IUserRepo, UserRepo>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
